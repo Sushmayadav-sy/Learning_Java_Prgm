5dd2017920ffe0e8ce90f8b28c94da1165162a52
SushmaYadav <sushmayadav.at.kolar@gmail.com>
2025-03-11 06:52:30 +0400

    File naming conventions, defining main method

---
+++ src/Ex_02_javaBasics_part2/Lab0014_Variables.java
@@ -0,0 +1,14 @@
+package Ex_02_javaBasics_part2;
+
+public class Lab0014_Variables {
+    public static void main(String[] args) {
+        // data_type variable_name = Variable_value
+
+        byte b =10;
+//        byte --> data type - type of container -
+//                b--> variable name --> identifier
+//                = --> assignment
+//        variable value = 10 -->
+
+    }
+}
