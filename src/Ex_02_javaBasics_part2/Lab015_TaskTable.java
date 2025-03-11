acb389a0ac9a0a369f057b0222ad9dd4975180c7
SushmaYadav <sushmayadav.at.kolar@gmail.com>
2025-03-11 07:00:20 +0400

    File naming conventions, defining main method

--- src/Ex_02_javaBasics_part2/Lab015_TaskTable.java
+++ src/Ex_02_javaBasics_part2/Lab015_TaskTable.java
@@ -4,44 +4,34 @@ public class Lab015_TaskTable {
     public static void main(String[] args) {
         int a =9;
         int b=1;
-        int c= a*b;
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
         b=b+1;
-        c= a*b;
         System.out.println();
-        System.out.printf("%d x %d = %d ", a,b,c);
+        System.out.printf("%d x %d = %d ", a,b,a*b);
 
 
     }
