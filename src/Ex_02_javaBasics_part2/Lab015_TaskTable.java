adc46591c683292e145ae29db5883c4e31c2a8c6
SushmaYadav <sushmayadav.at.kolar@gmail.com>
2025-03-11 06:51:20 +0400

    File naming conventions, defining main method

---
+++ src/Ex_02_javaBasics_part2/Lab015_TaskTable.java
@@ -0,0 +1,49 @@
+package Ex_02_javaBasics_part2;
+
+public class Lab015_TaskTable {
+    public static void main(String[] args) {
+        int a =9;
+        int b=1;
+        int c= a*b;
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+        b=b+1;
+        c= a*b;
+        System.out.println();
+        System.out.printf("%d x %d = %d ", a,b,c);
+
+
+    }
+
+}
