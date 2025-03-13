e87a3dfa88291972512af62ac1388ec7bab20b03
SushmaYadav <sushmayadav.at.kolar@gmail.com>
2025-03-13 05:35:47 +0400

    solving equation task 12th march

---
+++ src/tasks_javaClasses/Solving_equation.java
@@ -0,0 +1,12 @@
+package tasks_javaClasses;
+
+public class Solving_equation {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+        int z = 10;
+
+       double a = Math.cbrt(Math.pow(x, 2) + Math.pow(y, 2) - Math.abs(z));
+        System.out.printf("Result %f", a);
+    }
+    }
